d741343db9459ad68c6d42ce45b581ae56d28ad8
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-10-03 11:38:12 -0600

    Add project files.

---
+++ QueueInheritanceLibrary/QueueInheritance.cs
@@ -0,0 +1,24 @@
+﻿using LinkedListLibrary;
+
+namespace QueueInheritanceLibrary
+{
+    public class QueueInheritance : List
+    {
+        // Pass the name "queue" to the list constructor
+        public QueueInheritance() : base("queue") { }
+
+        // Place datsValue at the end of queue by inserting 
+        // at end of linked list
+        public void Enqueue(object dataValue)
+        {
+            InsertAtBack(dataValue);
+        }
+
+        // Remove item from front of queue by removing
+        // item at front of linked list
+        public object Dequeue()
+        {
+            return RemoveFromFront();
+        }
+    }
+}
